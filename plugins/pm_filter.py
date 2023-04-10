010d6429b622cec4df0f6e1db3ce0ce82aa6e048
spidyynaik <128451864+spidyynaik@users.noreply.github.com>
2023-04-10 15:19:59 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -592,14 +592,11 @@ async def cb_handler(client: Client, query: CallbackQuery):
     elif query.data == "start":
         buttons = [[
             InlineKeyboardButton('➕↖️ Add Me To Your Groups ↗️➕', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
-        ], [
-            InlineKeyboardButton('🧞‍♀️ Search 🧐', switch_inline_query_current_chat=''),
-            InlineKeyboardButton('🔔 Updates 🤖', url='https://t.me/LazyDeveloper')
-        ], [
+        ],[
             InlineKeyboardButton('🙆🏻 Help 🦾', callback_data='help'),
             InlineKeyboardButton('♥️ About ♥️', callback_data='about')
         ],[
-            InlineKeyboardButton('Join Update Channel', url='https://t.me/YourDemandZone')
+            InlineKeyboardButton('☢️Update Channel☢️', url='https://t.me/YourDemandZone')
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await query.message.edit_text(
